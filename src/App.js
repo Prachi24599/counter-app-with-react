22658ea6a10c87bb78c69b435d9b5ec52900abe4
Prachi24599 <prachi24599@gmail.com>
2023-06-11 22:36:21 +0530

    style added using tailwind

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
+import { useState } from "react";
 import "./App.css";
 import "./index.css";
 function App() {
   return (
     <div className="w-[100vw] h-[100vh] flex justify-center items-center bg-[#344151] flex-col gap-10">
-      <div>Increment && Decrement</div>
-      <div>
-        <button>-</button>
-        <div></div>
-        <button>+</button>
+      <div className="text-[#0398d4] font-medium text-2xl">
+        Increment & Decrement
       </div>
-      <button>Reset</button>
+      <div className="bg-white flex justify-center items-center gap-12 py-3 rounded-sm text-[25px] text-[#344151]">
+        <button
+          onClick={decrementHandler}
+          className="border-r-2 text-center w-20 border-[#bfbfbf] text-5xl"
+        >
+          -
+        </button>
+        <div className="font-bold gap-12 text-5xl">{count}</div>
+        <button
+          onClick={incrementHandler}
+          className="border-l-2 text-center w-20 border-[#bfbfbf] text-5xl"
+        >
+          +
+        </button>
+      </div>
+      <button
+        onClick={resetHandler}
+        className="bg-[#0398d4] text-white px-5 py-2 rounded-sm text-lg"
+      >
+        Reset
+      </button>
     </div>
   );
 }
